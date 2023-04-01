affb654cece9d8ece8110bb610dfcae62e461e25
Francesco Face <francesco.face@gmail.com>
2023-04-01 08:07:01 +0200

    Remove webpack and use a manual process to create both dev and production build

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,15 @@
 # When run through `npm run release` the script is executed from the project root, so all paths
 # are relative to that path
 npm run clean-release
-webpack --config webpack.prod.js
+
+# Compile typescript into dist/bin
+tsc --outDir dist/bin
+cp package.json dist/bin
+cd dist/bin
+npm install --omit=dev
+
+cd ../..
 find alfred-workflow -type f ! -name "prefs.plist" -exec cp {} dist/ \;
+
 cd dist
 zip -r alfred-phil.alfredworkflow *
